8f7df00c4862897e84592d3a211d8206086b081a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-10 21:49:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,27 +1,25 @@
 def oxford_comma(array)
-  array_length = array.length
-  case array_length
+  if array.length == 1
+    array.join
 
-    when 1
-      array.join
+  elsif array.length == 2
+    array.join(" and ")
 
-    when 2
-      array.join(" and ")
-    when array_length > 2
-      # get the last word from the array form of the phrase
-      phrase_last_word = array.pop
-      # return the array for of the phrase without the last word
-      #phrase_without_last_word = array
+  else
+    # get the last word from the array form of the phrase
+    phrase_last_word = array.pop
+    # return the array for of the phrase without the last word
+    #phrase_without_last_word = array
 
-      # test puts
-      #puts "#{phrase_without_last_word}"
-      #puts "#{phrase_last_word}"
+    # test puts
+    #puts "#{phrase_without_last_word}"
+    #puts "#{phrase_last_word}"
 
-      # join the phrase without the last word
-      array.join(", ")
+    # join the phrase without the last word
+    array.join(", ")
 
-      # add back the last word and "and"
-      phrase = "#{array} and #{phrase_last_word}"
+    # add back the last word and "and"
+    phrase = "#{array} and #{phrase_last_word}"
   end
 end
 #test_array = ["first", "second", "third", "fourth", "fifth"]
