2cc458d9eaf6287b523338126fc7772eeeb924bb
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-10 21:52:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,17 +8,10 @@ def oxford_comma(array)
   else
     # get the last word from the array form of the phrase
     phrase_last_word = array.pop
-    # return the array for of the phrase without the last word
-    #phrase_without_last_word = array
-
-    # test puts
-    #puts "#{phrase_without_last_word}"
-    #puts "#{phrase_last_word}"
-
     # join the phrase without the last word
-    string = array.join(", ")
+    phrase_without_last_word = array.join(", ")
     # add back the last word and "and"
-    phrase = "#{string}, and #{phrase_last_word}"
+    phrase = "#{phrase_without_last_word}, and #{phrase_last_word}"
   end
 end
 #test_array = ["first", "second", "third", "fourth", "fifth"]
